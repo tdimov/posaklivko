b438775ed6da860c8c5258e71d518375e99d83ab
Tihomir Dimov <tihomir.dimov@ecommera.com>
2016-02-25 16:04:52 +0200

    set restart task into build js

--- build/tasks/default.js
+++ build/tasks/default.js
@@ -2,5 +2,5 @@ import gulp from 'gulp';
 import run from 'run-sequence';
 
 gulp.task('default', cb => {
-  run('server', 'build', 'restart', 'watch', cb);
+  run('server', 'build', 'watch', cb);
 });
