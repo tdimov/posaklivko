aabd1624d6764132c43931cba58996b2b4493fd3
tdimov <tihomirdimov92@gmail.com>
2016-04-06 17:12:12 +0300

    renaming fileUploader variable

--- server/routes/homeRoutes.js
+++ server/routes/homeRoutes.js
@@ -4,7 +4,7 @@ import {FilesUploader} from '../utils/filesUploader';
 export class HomeRoutes {
   constructor (app) {
     this.homeController = new HomeController();
-    this.fileUploader = new FilesUploader();
+    this.filesUploader = new FilesUploader();
     this._init(app);
   }
 
@@ -15,7 +15,7 @@ export class HomeRoutes {
 
     app.get('/contact', this.homeController.getContact);
 
-    app.post('/contact', this.fileUploader.uploadMultipleFiles('files'),  this.homeController.postContact);
+    app.post('/contact', this.filesUploader.uploadMultipleFiles('files'),  this.homeController.postContact);
 
     app.get('/error', this.homeController.errorPage);
   }
